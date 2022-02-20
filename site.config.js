0092724185e3b2d6f2b4bf78585cf34d4451aa3e
Arthur <greatdwb@gmail.com>
2022-02-20 22:37:19 +0800

    update site config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '2fce558c9b264975afa4616d0e6b7492',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Arthur OneByte',
+  domain: 'arthurcode.com',
+  author: 'Arthur OneByte',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: 'interesting topics about techinques',
+  socialImageTitle: 'Arthur OneByte',
+  socialImageSubtitle: 'Connecting Dots... 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'ArthurOneByte',
+  github: 'GitBubble',
+  linkedin: '#',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
